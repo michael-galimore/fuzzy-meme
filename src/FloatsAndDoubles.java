97c5053d9de84f18c91854ec95d351258a75d569
mike <michael.galimore1@gmail.com>
2022-06-29 16:03:56 -0500

    Floats and Doubles

---
+++ src/FloatsAndDoubles.java
@@ -0,0 +1,23 @@
+public class FloatsAndDoubles {
+
+    public static void main(String[] args) {
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("Float Minimum Value = " + myMinFloatValue);
+        System.out.println("Float Maximum Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Double Minimum Value = " + myMinDoubleValue);
+        System.out.println("Double Maximum Value = " + myMaxDoubleValue);
+
+
+        int myIntValue = 7;
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5.25d;
+
+    }
+}
