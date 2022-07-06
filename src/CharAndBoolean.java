0d1250ab6812a7a74278e3080938b15f12891f8b
mike <michael.galimore1@gmail.com>
2022-07-06 13:46:47 -0500

    Char and Boolean

---
+++ src/CharAndBoolean.java
@@ -0,0 +1,24 @@
+public class CharAndBoolean {
+
+    public static void main(String[] args) {
+
+        char myChar = 'G';
+//        char stores only a single character.
+        char myUnicodeChar = '\u004D';
+        char myUnicodeChar2 = '\u0049';
+        char myUnicodeChar3 = '\u004B';
+        char myUnicodeChar4 = '\u0045';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        System.out.println(myUnicodeChar2 );
+        System.out.println(myUnicodeChar3);
+        System.out.println(myUnicodeChar4);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+
+    }
+}
