2e4b37069edd2bcdd92914e880347846983662d6
mike <michael.galimore1@gmail.com>
2022-07-06 17:47:11 -0500

    Abbreviating Operators

--- src/OperatorsOperandsExpressions.java
+++ src/OperatorsOperandsExpressions.java
@@ -24,6 +24,29 @@ public class OperatorsOperandsExpressions {
         result = result % 3; // the remainder of (4 % 3) = 1    % is the modulus operator
         System.out.println("4 % 3 = " + result);
 
+        //result = result + 1
+        result++;  // 1 + 1 = 2
+        System.out.println("1 + 1 = " + result);
+
+        result--;
+        System.out.println("2 - 1 = " + result);
+
+        //result +2
+        result += 2; // 1 + 2 = 3
+        System.out.println("1 + 2 = " + result);
+
+        //result = result * 10;
+        result *= 10; // 3 * 10 = 30
+        System.out.println("3 * 10 = " + result);
+
+        //result = result / 3
+        result /= 3;
+        System.out.println("30 / 3 = " + result);
+
+        //result = result - 2;
+        result -= 2;  // 10 - 2 = 8
+        System.out.println("10 - 2 = " + result);
+
 
     }
 
