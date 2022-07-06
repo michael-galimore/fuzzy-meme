107f01df7abc8e9b493a433f020a2e9be89c5a9e
mike <michael.galimore1@gmail.com>
2022-07-06 10:13:51 -0500

    Floats and Doubles Challenge

--- src/FloatsAndDoubles.java
+++ src/FloatsAndDoubles.java
@@ -15,9 +15,27 @@ public class FloatsAndDoubles {
         System.out.println("Double Maximum Value = " + myMaxDoubleValue);
 
 
-        int myIntValue = 7;
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5.25d;
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+//        Challenge
+//        1. Create a variable with the appropriate type to store the number of pounds to be converted to kilograms.
+//        2. Calculate the result i.e. the number of kilograms based on the contents of the variable above and store the result in a 2nd appropriate variable.
+//        3. Print out result.
+
+        double myPoundage = 15d;
+        double conversion = 0.45359237d;
+        double converted = myPoundage * conversion;
+        System.out.println(myPoundage + " pounds "+"equals " + converted + " kilograms!" );
+
+
+
+
+
 
     }
 }
