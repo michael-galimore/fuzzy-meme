047e3c4d2e400fbf109ca9b8b650ccf346e99798
mike <michael.galimore1@gmail.com>
2022-07-07 08:57:25 -0500

    Operators

--- src/OperatorsOperandsExpressions.java
+++ src/OperatorsOperandsExpressions.java
@@ -47,6 +47,37 @@ public class OperatorsOperandsExpressions {
         result -= 2;  // 10 - 2 = 8
         System.out.println("10 - 2 = " + result);
 
+        boolean isAlien = false;
+        if(isAlien == false) {
+            System.out.println("It is not an alien.");
+        }
+        int topScore = 80;
+        if(topScore <= 100){
+            System.out.println("You got the high score!");
+        }
+        int secondTopScore = 81;
+        if((topScore > secondTopScore) && (topScore < 100)){
+            System.out.println("Greater than top score and less than 100");
+        }
+        if((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if(newValue == 50){
+            System.out.println("This is true.");
+        }
+
+        boolean isCar = false;
+        if(isCar == false){
+            //if(!isCar) can be used as well
+            System.out.println("This is not suppose to happen.");
+        }
+
+
+
+
+
 
     }
 
