51f3980c227c4d47e68e1e81b5b5e937ec52f81b
mike <michael.galimore1@gmail.com>
2022-07-14 16:56:07 -0500

    Keyword and Expressions

---
+++ src/StatementsWhitespaceAndIndention.java
@@ -0,0 +1,15 @@
+public class StatementsWhitespaceAndIndention {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+        " another"
+        + " statement");
+
+//       These are all statements. Java doesn't mind whitespaces. Indenting helps make code more readable.
+    }
+}
