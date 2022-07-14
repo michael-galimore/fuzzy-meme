c33e040a6f6a201dfe88b0ae0c6b5ac08e3f294c
mike <michael.galimore1@gmail.com>
2022-07-14 16:37:05 -0500

    Keyword and Expressions

---
+++ src/KeywordAndExpressions.java
@@ -0,0 +1,14 @@
+public class KeywordAndExpressions {
+
+    public static void main(String[] args) {
+//        a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344d);
+        int highScore = 100;
+
+        if(highScore >= 50){
+            System.out.println("This is an expression");
+        } else{
+            System.out.println("This is also an expression");
+        }
+    }
+}
