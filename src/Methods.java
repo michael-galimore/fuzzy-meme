91a8f2928f718caa7293d1706240d84bc26284e5
mike <michael.galimore1@gmail.com>
2022-07-19 13:26:18 -0500

    More on Methods

--- src/Methods.java
+++ src/Methods.java
@@ -1,11 +1,22 @@
 public class Methods {
 
     public static void main(String[] args) {
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int highScore =  calculate(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
 
 
-        calculate(true,800,5,100);
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
 
-        calculate(true, 10000, 8, 200);
+        highScore =  calculate(gameOver, score, levelCompleted, bonus);
+        System.out.println("Your final score was " + highScore);
+
 
     }
         public static int calculate(boolean gameOver, int score, int levelCompleted, int bonus){
@@ -13,7 +24,7 @@ public class Methods {
             if(gameOver){
                 int finalScore = score + (levelCompleted * bonus);
                 finalScore += 1000;
-                System.out.println("Your final score was " + finalScore);
+
                 return finalScore;
             } else {
                 return -1;
