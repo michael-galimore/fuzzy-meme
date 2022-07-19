e485a96fe2fe96a005b1969e90b9a273a072991e
mike <michael.galimore1@gmail.com>
2022-07-19 17:04:52 -0500

    Method Challenges

--- src/Methods.java
+++ src/Methods.java
@@ -10,12 +10,45 @@ public class Methods {
         System.out.println("Your final score was " + highScore);
 
 
+
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
 
         highScore =  calculate(gameOver, score, levelCompleted, bonus);
         System.out.println("Your final score was " + highScore);
+
+
+        // Create a method called displayHighScorePosition
+// it should show a players name as a parameter and 2nd parameter as a position in the high score table
+// You should display the players name along with a message like " manged to get into position " and the position they got and a further message "on the high score table".
+
+//        Create a 2nd method called calculateHighScorePosition
+//        it should be sent one argument only, the player score
+//        it should return an int
+//        the return data should be
+//        1 if the score is > 1000
+//        1 if the score is > 500 and < 1000
+//        3 if the score is > 100 and < 500
+//        4 in all other cases
+//        call both methods and display the results of the following
+//        a score of 1500, 900, 400, and 50
+
+
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Mike", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(900);
+        displayHighScorePosition("Kase", highScorePosition);
+
+
+        highScorePosition = calculateHighScorePosition(400);
+        displayHighScorePosition("Meredith", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Bobby", highScorePosition);
+
+
 
 
     }
@@ -29,5 +62,19 @@ public class Methods {
             } else {
                 return -1;
             }
+        }
+
+        public static void displayHighScorePosition(String playersName, int highScorePosition){
+            System.out.println(playersName+ " managed to get into position " + highScorePosition + " on the high score table.");
+        }
+
+        public static int calculateHighScorePosition(int playerScore){
+        if(playerScore >= 1000){
+            return 1;
+        } else if (playerScore >= 500 && playerScore < 1000){
+            return 2;
+        } else if(playerScore >= 100 && playerScore < 500){
+            return 3;
+        } return 4;
         }
 }
