48ddcc35972a43c8eedbe06e3012c923e4832849
mike <michael.galimore1@gmail.com>
2022-07-19 11:16:37 -0500

    Methods in Java

--- src/Methods.java
+++ src/Methods.java
@@ -1,22 +1,11 @@
 public class Methods {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
 
         calculate(true,800,5,100);
 
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
+        calculate(true, 10000, 8, 200);
 
-        if(gameOver){
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
     }
         public static void calculate(boolean gameOver, int score, int levelCompleted, int bonus){
 
