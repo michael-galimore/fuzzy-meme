3b1df88bb9d4bd6d4d3c2f327195d58669f4a792
mike <michael.galimore1@gmail.com>
2022-07-19 13:10:28 -0500

    Methods in Java

--- src/Methods.java
+++ src/Methods.java
@@ -2,19 +2,21 @@ public class Methods {
 
     public static void main(String[] args) {
 
+
         calculate(true,800,5,100);
 
         calculate(true, 10000, 8, 200);
 
     }
-        public static void calculate(boolean gameOver, int score, int levelCompleted, int bonus){
+        public static int calculate(boolean gameOver, int score, int levelCompleted, int bonus){
 
             if(gameOver){
-//            if(gameOver)
-//            finalScore is a scoped variable meaning that it can only be accessed inside the code block.
                 int finalScore = score + (levelCompleted * bonus);
                 finalScore += 1000;
                 System.out.println("Your final score was " + finalScore);
+                return finalScore;
+            } else {
+                return -1;
             }
         }
 }
