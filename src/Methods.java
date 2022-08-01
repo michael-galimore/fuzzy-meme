2037ff8752a39c507cf2e27d8d783dda3a3db299
mike <michael.galimore1@gmail.com>
2022-08-01 10:35:28 -0500

    Methods with refactor

--- src/Methods.java
+++ src/Methods.java
@@ -68,13 +68,26 @@ public class Methods {
             System.out.println(playersName+ " managed to get into position " + highScorePosition + " on the high score table.");
         }
 
-        public static int calculateHighScorePosition(int playerScore){
-        if(playerScore >= 1000){
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000){
-            return 2;
-        } else if(playerScore >= 100 && playerScore < 500){
-            return 3;
-        } return 4;
+        public static int calculateHighScorePosition(int playerScore) {
+
+//            if (playerScore >= 1000) {
+//                return 1;
+//            } else if (playerScore >= 500) {
+//                return 2;
+//            } else if (playerScore >= 100) {
+//                return 3;
+//            }
+//            return 4;
+            int position = 4; // assuming position 4 will be returned
+
+            if(playerScore >= 1000){
+                position = 1;
+            } else if (playerScore >= 500) {
+                position = 2;
+            } else if (playerScore >= 100) {
+                position = 3;
+            }
+                return position;
+
         }
 }
